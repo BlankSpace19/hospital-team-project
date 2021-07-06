e33010024dcc85b04f40fd8422e70454cd6d5017
BlankSpace19 <80050462+BlankSpace19@users.noreply.github.com>
2021-07-06 16:36:52 +0900

    hospital-2021-07-06

---
+++ script/script.js
@@ -0,0 +1,26 @@
+
+//1. bxslider
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+      auto: true, //자동으로 애니메이션 시작
+      speed: 500, //애니메이션 속도
+      pause: 3000, //애니메이션 유지 시간 (1000은 1초)
+      mode: 'horizontal', //슬라이드 모드('fade', 'horizontal', 'vertical' 이 있음)
+      autoControls: false, //시작 및 중지버튼 보여짐
+      pager: false, //페이지 표시 보여짐
+      captions: false //이미지 위에 텍스트를 넣을 수 있음
+    });
+  });
+
+$(function(){
+    //2. navi
+    $('.screen-header__navi__depth01 > li').mouseenter(function(){
+        $('.screen-header__navi__depth02').stop().slideDown();
+        $('.screen-header__navi__background').stop().slideDown();
+    });
+    $('.screen-header__navi__depth01 > li').mouseleave(function(){
+        $('.screen-header__navi__depth02').stop().slideUp();
+        $('.screen-header__navi__background').stop().slideUp();
+    });
+    
+});
